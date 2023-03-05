98983ca5f3eb70e8d18073813688aab63f418685
boy-son <branden.wheat@yahoo.com>
2023-03-05 00:23:37 -0500

    filled out some user files in public/js

---
+++ public/js/signup.js
@@ -0,0 +1,23 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    if (username && password) {
+      const response = await fetch('/register', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+        console.log(response);
+      }
+      const data = await response.json();
+      console.log(data);
+    }
+  };
+  document.querySelector('#signUp').addEventListener('submit', signupFormHandler); 
